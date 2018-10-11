ce39d141a86da94b9d643191d2df85b382d176f4
billm79 <bill.m79@student.parishepiscopal.org>
2018-10-11 18:43:11 -0500

    added instructions, debug statements, and remove_spaces()

--- misc/slope_of_curve.py
+++ misc/slope_of_curve.py
@@ -19,10 +19,34 @@
 
 
 def main():
+    print("\nThis program will find the slope of a curve at a point. The equation may be entered")
+    print("as it would be written, with a few exceptions. There are no implicit operations.")
+    print("For example, multiplication (e.g. 2x) must be specified using an asterisk (e.g. 2*x).")
+    print("Exponentiation is indicated using the caret symbol, ^ (e.g. x squared is x^2).")
+    print("Do not enter 'y='.\n")
     expr = input("Enter equation: y = ")
+    print(expr)     # debug
+
+    # remove spaces
+    expr = remove_spaces(expr)
+    print(expr)     # debug
+
+    # debug--begin: print position numbers below expr to help debugging
     for i in range(len(expr)):
+        print('0' if i < 10 else '1', end='')
+    print()
+    for i in range(len(expr)):
+        print(i % 10, end='')
+    print()
+    # debug--end
+
+    # determine coefficients and exponents
+    for i in range(len(expr)):
+        # consider a single character in expr
         c = expr[i]
-        if c == 'x' and i != 0:
+
+        # if 'x' is found, find coefficient and exponent
+        if c == 'x' and i != 0:                             # 'x' found
             x_loc = i
 
             # find coefficient
@@ -40,6 +64,14 @@ def is_contained_in(char, str_list):
         if char == c:
             return True
     return False
+
+
+def remove_spaces(s):
+    ret_str = ''
+    for c in s:
+        if c != ' ':
+            ret_str += c
+    return ret_str
 
 
 main()
