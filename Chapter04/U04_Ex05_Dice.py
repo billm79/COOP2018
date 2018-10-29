dd0a1e70be731c2d1f20f8b6b669e4d2899ff5ac
billm79 <bill.m79@student.parishepiscopal.org>
2018-10-29 09:24:08 -0500

    minor changes, mainly documentation

--- Chapter04/U04_Ex05_Dice.py
+++ Chapter04/U04_Ex05_Dice.py
@@ -27,21 +27,34 @@ from U04_Ex04_WinterScene import drawCircle
 def main():
     win = GraphWin("Dice", 560, 120)
     win.setBackground("green4")
-    for i in range(1,7):
+
+    # draw dies 1 - 5 for straight
+    for i in range(1, 6):
         drawDie(win, i)
     win.getMouse()
 
 def drawDie(win, rank):
+    """
+    Draws a single die of given rank
+    Algorithm: 1) draw rectangle for die; 2) draw pips based on rank
+    :param win: GraphWin object
+    :param rank: int -> rank of die
+    """
     rect = Rectangle(Point(10 + 110*(rank - 1), 10), Point(110 + 110*(rank - 1), 110))
     rect.setOutline("black")
     rect.setFill("white")
     rect.draw(win)
+
+    # odd ranks have a center pip
     if rank % 2 == 1:
         drawPipCenter(win, rect.getCenter())
+
+    # ranks 2 and 3 have top left and bottom right pips
     if rank == 2 or rank == 3:
         drawPipTL(win, rect.getCenter())
         drawPipBR(win, rect.getCenter())
 
+    # ranks 4 and 5 have all four corner pips
     if rank == 4 or rank == 5:
         drawPipTL(win, rect.getCenter())
         drawPipBL(win, rect.getCenter())
@@ -49,18 +62,43 @@ def drawDie(win, rank):
         drawPipBR(win, rect.getCenter())
 
 def drawPipCenter(win, dieCenter):
+    """
+    Draws center pip relative to die center
+    :param win: GraphWin object
+    :param dieCenter: Point -> center point of die
+    """
     drawCircle(dieCenter, 7, "black", "black").draw(win)
 
 def drawPipTL(win, dieCenter):
+    """
+    Draws top left pip relative to die center
+    :param win: GraphWin object
+    :param dieCenter: Point -> center point of die
+    """
     drawCircle(Point(dieCenter.getX() - 30, dieCenter.getY() - 30), 7, "black", "black").draw(win)
 
 def drawPipTR(win, dieCenter):
+    """
+    Draws top right pip relative to die center
+    :param win: GraphWin object
+    :param dieCenter: Point -> center point of die
+    """
     drawCircle(Point(dieCenter.getX() + 30, dieCenter.getY() - 30), 7, "black", "black").draw(win)
 
 def drawPipBL(win, dieCenter):
+    """
+    Draws bottom left pip relative to die center
+    :param win: GraphWin object
+    :param dieCenter: Point -> center point of die
+    """
     drawCircle(Point(dieCenter.getX() - 30, dieCenter.getY() + 30), 7, "black", "black").draw(win)
 
 def drawPipBR(win, dieCenter):
+    """
+    Draws bottom right pip relative to die center
+    :param win: GraphWin object
+    :param dieCenter: Point -> center point of die
+    """
     drawCircle(Point(dieCenter.getX() + 30, dieCenter.getY() + 30), 7, "black", "black").draw(win)
 
 main()
