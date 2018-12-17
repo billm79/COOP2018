f50dcc4cd2e08b68fdb8a05d4314850986f43b17
billm79 <bill.m79@student.parishepiscopal.org>
2018-12-16 20:38:53 -0600

    added strip() to remove non letters

--- Chapter05/U05_Ex10_AvgWordLen.py
+++ Chapter05/U05_Ex10_AvgWordLen.py
@@ -19,12 +19,14 @@
 #   Print intro
 #   Get sentence from user
 #   Split sentence into words
-#   Accumulate total length of words
+#   Accumulate total length of words (stripping non-letters)
 #   Divide by number of words to get average word length
 #   Print results
 
 
 def main():
+    avgWrdLen = 0
+
     # Print intro
     print('This program calculates the average word length in a sentence entered by the user.')
 
@@ -32,17 +34,34 @@ def main():
     inputStr = input('Please enter a sentence: ')
 
     # Split sentence into words
-    words = inputStr.split(' ')
+    words = inputStr.split()
 
     # Accumulate total length of words
     ttlLen = 0
     for word in words:
+        word = strip(word)
+        # print("[{}]: {}".format(word, len(word)))   # uncomment to see "[word]: len(word)"
         ttlLen += len(word)
 
     # Divide by number of words to get average word length
         avgWrdLen = ttlLen / len(words)
 
     # Print results
-    print('    Number of words: {0}\nAverage word length: {1:.1f}'.format(len(words), avgWrdLen))
+    print('    Number of words: {0}\nAverage word length: {1:.2f}'.format(len(words), avgWrdLen))
+    
+
+def strip(word):
+    """
+    consider each character of lower case word, adding it to a return value if between 'a' and 'z', inclusive
+    :param word: str -> a word
+    :return: str -> stripped word
+    """
+    stripped = ''
+    
+    for char in word.lower():
+        if char >= 'a' and char <= 'z':
+            stripped += char
+    return stripped
+
 
 main()
