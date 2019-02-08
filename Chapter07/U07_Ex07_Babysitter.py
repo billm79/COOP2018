757f324322e08878676df87b66a269ff617d6019
billm79 <bill.m79@student.parishepiscopal.org>
2019-02-08 16:49:45 -0600

    added unit test results

--- Chapter07/U07_Ex07_Babysitter.py
+++ Chapter07/U07_Ex07_Babysitter.py
@@ -24,9 +24,11 @@
 #   apply hourly rate of $1.75 for time after 21:00, prorate partial hours
 #   print resulting babysitting bill
 
+EARLY_RATE = 2.5
+LATE_RATE  = 1.75
 
-def sitterBill(earlyRate, lateRate):
-    # earlyRate and lateRate are parameters
+
+def main():
     # introduce program
     print('\nThis program calculates babysitting bill based on hourly rates, start time, and end time.')
 
@@ -34,20 +36,40 @@ def sitterBill(earlyRate, lateRate):
     start = input('\nWhat time did the babysitter arrive? (hh:mm 24-hr) ')
     end = input('What time did the babysitter leave? (hh:mm 24-hr) ')
 
+    sitterBillAmt = sitterBill(start, end)
+
+    # print resulting babysitting bill
+    print('\nTotal babysitting bill is ${0:0.2f}.'.format(sitterBillAmt))
+
+
+def sitterBill(start, end):
+    # earlyRate and lateRate are parameters
     # parse into hour and minute variables for start and end
     startTime = start.split(':'); startNum = float(startTime[0]) + float(startTime[1]) / 60
     endTime = end.split(':'); endNum = float(endTime[0]) + float(endTime[1]) / 60
 
     # apply hourly rate of $2.50 for time before 21:00, prorate partial hours
     if startNum < 21 and endNum < 21:
-        sitterBillAmt = (endNum - startNum) * earlyRate
+        sitterBillAmt = (endNum - startNum) * EARLY_RATE
     # apply hourly rate of $1.75 for time after 21:00, prorate partial hours
     elif startNum >= 21 and endNum >= 21:
-        sitterBillAmt = (endNum - startNum) * lateRate
+        sitterBillAmt = (endNum - startNum) * LATE_RATE
     else:
-        sitterBillAmt = (21 - startNum) * earlyRate + (endNum - 21) * lateRate
+        sitterBillAmt = (21 - startNum) * EARLY_RATE + (endNum - 21) * LATE_RATE
 
-    # print resulting babysitting bill
-    print('\nTotal babysitting bill is ${0:0.2f}.'.format(sitterBillAmt))
+    return sitterBillAmt
 
-sitterBill(2.5, 1.75)
+
+if __name__ == '__main__':
+    main()
+
+
+'''
+RESULTS:
+========
+sitterBill("18:00", "20:00")   -->   5.0 |   5.0 | [ Pass ]
+sitterBill("19:00", "21:00")   -->   5.0 |   5.0 | [ Pass ]
+sitterBill("19:00", "23:00")   -->   8.5 |   8.5 | [ Pass ]
+sitterBill("21:00", "23:00")   -->   3.5 |   3.5 | [ Pass ]
+========
+'''
