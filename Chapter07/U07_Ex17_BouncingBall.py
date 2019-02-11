65f160c53d9abdd3a1896cc36184bd68fe2bc1d7
billm79 <bill.m79@student.parishepiscopal.org>
2019-02-10 19:19:54 -0600

    improved edge bounce

--- Chapter07/U07_Ex17_BouncingBall.py
+++ Chapter07/U07_Ex17_BouncingBall.py
@@ -27,8 +27,21 @@
 
 from graphics import *
 from random import *
+from os import system
+from sys import platform
+
+topBound = 0
+rightBound = 0
+bottomBound = 0
+leftBound = 0
+win = None
+SILENT = True
+flashBar = Rectangle(Point(0,0), Point(0,0))
+FLASH = False
 
 def main():
+    global topBound, rightBound, bottomBound, leftBound, win
+
     # create GraphWin
     win = GraphWin('Bouncing Ball', 600, 600)
     rightBound = win.getWidth() / 2
@@ -60,11 +73,34 @@ def main():
     for i in range(10000):
         count.setText(str(i))
 
+        # if current dx or dy will move bally beyond window edge, make adjustments
+        if abs(ball.getCenter().getX() + dx) >= rightBound:
+            if dx < 0:
+                dx = leftBound - ball.getCenter().getX() + radius
+                flash('L')
+                beep('L')
+            else:
+                dx = rightBound - ball.getCenter().getX() - radius
+                flash('R')
+                beep('R')
+
+        if abs(ball.getCenter().getY() + dy) >= topBound:
+            if dy < 0:
+                dy = bottomBound - ball.getCenter().getY() + radius
+                flash('B')
+                beep('B')
+            else:
+                dy = topBound - ball.getCenter().getY() - radius
+                flash('T')
+                beep('T')
+
+
         # move circle with current dx and dy until an edge is hit (dx or dy is within one radius of edge)
         ball.move(dx, dy)
 
         # randomly select a new dx or dy with opposite sign
         if dx <= 0 and ball.getCenter().getX() - leftBound <= radius:
+            # print('dx: {} | CenterX: {} | leftBound: {} | radius: {}'.format(dx, ball.getCenter().getX(), leftBound, radius))
             dx = randrange(10, 100)
         if dx >= 0 and rightBound - ball.getCenter().getX() <= radius:
             dx = randrange(-100, -10)
@@ -79,6 +115,34 @@ def main():
 
         # update(30) to slow execution to 30 fps
         update(30)
+
+
+def beep(dir):
+    if not SILENT:
+        if platform == 'darwin':
+            system('say {} &'.format('left' if dir == 'L' else 'right' if dir == 'R' else 'top' if dir == 'T' else 'bottom'))
+        else:
+            print('L\a')
+
+
+def flash(side):
+    global flashBar
+    widthFactor = 50
+
+    if FLASH:
+        if side == 'T':
+            flashBar = Rectangle(Point(leftBound, topBound), Point(rightBound, topBound - win.getHeight()/widthFactor))
+        elif side == 'R':
+            flashBar = Rectangle(Point(rightBound - win.getWidth()/widthFactor, topBound), Point(rightBound, bottomBound))
+        elif side == 'B':
+            flashBar = Rectangle(Point(leftBound, bottomBound + win.getHeight()/widthFactor), Point(rightBound, bottomBound))
+        else:
+            flashBar = Rectangle(Point(leftBound, topBound), Point(leftBound + win.getWidth()/widthFactor, bottomBound))
+
+        flashBar.setFill('red')
+        flashBar.setOutline('red')
+        flashBar.draw(win)
+        flashBar.undraw()
 
 
 main()
